9fa0296354b71b302ec1a9adc04bd1445d990d53
fix-it-central2021 <86719392+fix-it-central2021@users.noreply.github.com>
2021-07-15 21:32:00 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
   <MuiThemeProvider theme={THEME}>
     <Router>
 
-      <Cabecera titulo="FIX IT" />
+      <Cabecera titulo="PROYECTO VIRTUALIZACION" />
       <Barra></Barra>
       <div id="page-container">
         <div id="content-wrap">
